c9a6b9229995ea81bf61a9eb017898402ee2f2d9
Kenny Cordero <geckob4i@gmail.com>
2023-04-06 16:21:20 -0500

    created a 'books' field that the user can store their books in

--- server/models/User.js
+++ server/models/User.js
@@ -1,35 +1,30 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   name: {
     type: String
   },
-
   location: {
     type: String
   },
-
   email: {
     type: String,
     required: true,
   },
-
   password: {
     type: String,
     required: true
-  }
+  },
+  books: [{
+    title: String,
+    author: String,
+    price: Number,
+    cover_id: Number,
+    edition_count: Number,
+    first_publish_year: Number,
+    subject: [String]
+  }]
 });
-
-userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-userSchema.methods.isValidPassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
-}
 
 const User = model('User', userSchema);
 module.exports = User;
